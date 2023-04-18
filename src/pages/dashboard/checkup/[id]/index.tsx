66a004efc82b38dd7e0ecbe170a29695b29ca390
rayhan889 <raydev1604@gmail.com>
2023-04-18 16:55:26 +0700

    styling checkup details

--- src/pages/dashboard/checkup/[id]/index.tsx
+++ src/pages/dashboard/checkup/[id]/index.tsx
@@ -3,17 +3,29 @@ import type { PasienPlusPage } from "@/pages/_app";
 import { generateSSGHelper } from "@/server/api/helpers/ssgHelper";
 import { prisma } from "@/server/db";
 import { api } from "@/utils/api";
+import { useState } from "react"
 import Head from "next/head";
 import type { GetStaticPaths, GetStaticPropsContext } from "next/types";
 import type { RouterOutputs } from "@/utils/api";
 import { PatientDescription } from "./new";
 import { Spinner } from "@/components/ui/loading-overlay";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
 
 type PatientInfo = NonNullable<RouterOutputs["record"]['getRecordById']>['patient']
 
 const CheckupDetail: PasienPlusPage<{ id: string }> = ({ id }) => {
     const { data: report, isLoading } = api.record.getRecordById.useQuery({ id })
+
+    const [activeSlide, setActiveSlide] = useState(0)
 
     if (isLoading) {
         return <div className="flex justify-center h-full items-center">
@@ -26,6 +38,18 @@ const CheckupDetail: PasienPlusPage<{ id: string }> = ({ id }) => {
             <h1 className="scroll-m-20 text-xl font-extrabold tracking-tight">Data pemeriksaan tidak ada</h1>
         </div>
     }
+    console.log(report);
+
+    const attachment = report?.Attachment;
+
+    function goToPrevSlide (){
+        setActiveSlide((activeSlide - 1 + attachment.length) % attachment.length);
+    }
+
+    function goToNextSlide (){
+        setActiveSlide((activeSlide + 1) % attachment.length);
+    }
+
     return (
         <>
             <Head>
@@ -35,7 +59,119 @@ const CheckupDetail: PasienPlusPage<{ id: string }> = ({ id }) => {
                 <PatientDescription {...report?.patient as PatientInfo} />
                 <div className="bg-white overflow-hidden shadow sm:rounded-lg outline outline-1 outline-slate-200">
                     <div className="px-4 py-5 sm:p-6">
-
+                        <div className="max-w-full flex flex-col gap-6">
+                            <div className="w-full flex gap-4 items-center">
+                                <div className="w-1/5 flex justify-between items-center font-medium text-gray-500 text-sm">
+                                    <dt>Complaint </dt>
+                                    <dt>:</dt>
+                                </div>
+                                <div className="flex-1">
+                                    <dd className="text-gray-900 text-sm">{report?.complaint ?? "Belum ada komplain"}</dd>
+                                </div>
+                            </div>
+                            <div className="w-full flex gap-4 items-center">
+                                <div className="w-1/5 flex justify-between items-center font-medium text-gray-500 text-sm">
+                                    <dt>Diagnosis </dt>
+                                    <dt>:</dt>
+                                </div>
+                                <div className="flex-1">
+                                    <dd className="text-gray-900 text-sm">{report?.diagnosis ?? "Belum ada diagnosis"}</dd>
+                                </div>
+                            </div>
+                            <div className="w-full flex gap-4 items-center">
+                                <div className="w-1/5 flex justify-between items-center font-medium text-gray-500 text-sm">
+                                    <dt>Checkup </dt>
+                                    <dt>:</dt>
+                                </div>
+                                <div className="flex-1">
+                                    <dd className="text-gray-900 text-sm">{report?.checkup ?? "Belum ada checkup"}</dd>
+                                </div>
+                            </div>
+                            <div className="w-full flex gap-4 items-center">
+                                <div className="w-1/5 flex justify-between items-center font-medium text-gray-500 text-sm">
+                                    <dt>Treatment </dt>
+                                    <dt>:</dt>
+                                </div>
+                                <div className="flex-1">
+                                    <dd className="text-gray-900 text-sm">{report?.treatment ?? "Belum ada treatment"}</dd>
+                                </div>
+                            </div>
+                            <div className="w-full flex gap-4 items-center">
+                                <div className="w-1/5 flex justify-between items-center font-medium text-gray-500 text-sm">
+                                    <dt>Catatan Lab </dt>
+                                    <dt>:</dt>
+                                </div>
+                                <div className="flex-1">
+                                    <dd className="text-gray-900 text-sm">{report?.labNote ?? "Belum ada catatan lab"}</dd>
+                                </div>
+                            </div>
+                            <div className="w-full flex gap-4 items-center">
+                                <div className="w-1/5 flex justify-between items-center font-medium text-gray-500 text-sm">
+                                    <dt>Nominal Bayar </dt>
+                                    <dt>:</dt>
+                                </div>
+                                <div className="flex-1">
+                                    <dd className="text-gray-900 text-sm">{report?.pay.toLocaleString('id-ID',  { style: 'currency', currency: 'IDR' }) ?? "Belum ada nominal"}</dd>
+                                </div>
+                            </div>
+                            <div className="w-full flex gap-4 items-start">
+                                <div className="w-1/5 flex justify-between items-center font-medium text-gray-500 text-sm">
+                                    <dt>Attachment </dt>
+                                    <dt>:</dt>
+                                </div>
+                                <div className="flex-1 max-w-sm">
+                                    <div className="flex items-center justify-start">
+                                        {attachment.map((item, i) => (
+                                            <div 
+                                                key={i}
+                                                className={`${
+                                                    i === activeSlide
+                                                    ? "opacity-100 z-10"
+                                                    : "opacity-0 z-0 absolute"
+                                                } transition-opacity duration-500 ease-in-out w-full max-w-sm rounded-sm border p-2 border-gray-200`}
+                                            >
+                                                <div className="relative">
+                                                    <Image 
+                                                        src={item?.File?.url as string }
+                                                        alt={item?.File?.name as string}
+                                                        width={576}
+                                                        height={20}
+                                                        className="rounded-sm max-h-52 h-full object-cover hover:brightness-75 cursor-pointer"
+                                                    />
+                                                        <div className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity">
+                                                            <Dialog>
+                                                                <DialogTrigger>
+                                                                    <Button size="sm" variant="solidWhite">Lihat Detail</Button>
+                                                                </DialogTrigger>
+                                                                    <DialogContent className="max-w-3xl w-full">
+                                                                        <DialogHeader>
+                                                                            <DialogTitle>Attachment Detail</DialogTitle>
+                                                                            <DialogDescription>
+                                                                                <Image 
+                                                                                    src={item?.File?.url as string }
+                                                                                    alt={item?.File?.name as string} 
+                                                                                    width={576} 
+                                                                                    height={20}
+                                                                                    className="max-h-96 object-cover h-full"
+                                                                                />
+                                                                            </DialogDescription>
+                                                                        </DialogHeader>
+                                                                    </DialogContent>
+                                                            </Dialog>
+                                                        </div>
+                                                </div>
+                                            </div>
+                                        ))}
+                                    </div>
+                                    <div className="flex items-center justify-between mt-3">
+                                        <Button size="sm" variant="ghost" onClick={goToPrevSlide}>Prev</Button>
+                                        <p className="text-sm font-medium text-gray-600">{activeSlide + 1} / {attachment.length}</p>
+                                        <Button size="sm" variant="ghost" onClick={goToNextSlide}>Next</Button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
