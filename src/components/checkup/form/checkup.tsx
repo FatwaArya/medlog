ed06bcf1130c7329bf8de5c8e23eb51a225c7e98
rayhan889 <raydev1604@gmail.com>
2023-04-20 16:06:19 +0700

    revamp form checkup

--- src/components/checkup/form/checkup.tsx
+++ src/components/checkup/form/checkup.tsx
@@ -53,7 +53,221 @@ export function CheckupForm() {
 
     return (
         <div>
-            <div className="md:grid md:grid-cols-3 md:gap-6">
+            <div className="bg-white overflow-hidden sm:rounded-lg outline outline-1 outline-slate-200 mb-4 rounded-sm">
+                <div className="px-4 py-5 sm:px-6">
+                    <h3 className="text-lg leading-6 font-medium text-blue-600">Data Checkup</h3>
+                    <p className="mt-1 max-w-2xl text-sm text-gray-500">
+                        Isi data pasien disini dengan benar, agar pencarian data
+                        pasien lebih mudah.
+                    </p>
+                </div>
+                <div className="px-4 pb-6 pt-1 sm:pt-5 sm:px-6">
+                    <div className="md:grid md:grid-cols-1 md:gap-6">
+                    <div className="mt-5 md:col-span-2 md:mt-0">
+                        <div>
+                            <div className="grid grid-cols-6 gap-6">
+                                <div className="col-span-6">
+                                    <label
+                                        htmlFor="complaint"
+                                        className="block text-sm font-medium text-gray-700"
+                                    >
+                                        Keluhan Pasian {redAsterisk}
+                                    </label>
+                                    <div className="mt-1">
+                                        <textarea
+                                            id="complaint"
+                                            rows={3}
+                                            className="block w-full rounded-md border border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                                            placeholder="Patient had slight headache, and felt dizzy"
+                                            defaultValue={""}
+                                            {...register('complaint', { required: true })}
+                                        />
+                                    </div>
+                                </div>
+
+                                <div className="col-span-6 sm:col-span-3">
+                                    <label
+                                        htmlFor="treatment"
+                                        className="block text-sm font-medium text-gray-700"
+                                    >
+                                        Terapi {redAsterisk}
+                                    </label>
+                                    <div className="mt-1">
+                                        <Controller
+                                            name='treatment'
+                                            control={control}
+                                            render={({ field }) => (
+                                                <CreatableSelect
+                                                    {...field}
+                                                    isMulti
+                                                    isDisabled={isLoading}
+                                                    isLoading={isLoading}
+                                                    onChange={(value) => field.onChange(value)}
+                                                    onCreateOption={handleCreate}
+                                                    value={field.value}
+                                                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                                                    // @ts-ignore
+                                                    options={medicineOptions}
+                                                    styles={{
+                                                        input: (base) => ({
+                                                            ...base,
+                                                            'input:focus': {
+                                                                boxShadow: 'none',
+                                                            },
+                                                        }),
+                                                    }}
+                                                    formatCreateLabel={(inputValue) => `Tambahkan obat "${inputValue}"`}
+                                                />
+                                            )}
+                                        />
+                                    </div>
+                                </div>
+
+                                <div className="col-span-6 sm:col-span-3">
+                                    <label
+                                        htmlFor="fee"
+                                        className="block text-sm font-medium text-gray-700"
+                                    >
+                                        Fee {redAsterisk}
+                                    </label>
+                                    <div className="relative mt-1 rounded-md shadow-sm">
+                                        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                                            <span className="text-gray-500 sm:text-sm">IDR</span>
+                                        </div>
+                                        <Input
+                                            type="number"
+                                            id="fee"
+                                            min={0}
+                                            {...register("pay", {
+                                                required: true,
+                                                valueAsNumber: true,
+                                            })}
+                                            className="block w-full rounded-md border-gray-300 bg-white pl-11 pr-12 focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                                        />
+                                    </div>
+                                </div>
+
+                                <div className="col-span-6 sm:col-span-3">
+                                    <label
+                                        htmlFor="checkup"
+                                        className="block text-sm font-medium text-gray-700"
+                                    >
+                                        Pemeriksaan {redAsterisk}
+                                    </label>
+                                    <div className="mt-1">
+                                        <textarea
+                                            id="checkup"
+                                            rows={3}
+                                            className="block w-full rounded-md border border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                                            placeholder="Patient maybe had a migraine, and need to take paracetamol"
+                                            defaultValue={""}
+                                            {...register('checkup', { required: true })}
+                                        />
+                                    </div>
+                                </div>
+
+                                <div className="col-span-6 sm:col-span-3">
+                                    <label
+                                        htmlFor="notes"
+                                        className="block text-sm font-medium text-gray-700"
+                                    >
+                                        Catatan {redAsterisk}
+                                    </label>
+                                    <div className="mt-1">
+                                        <textarea
+                                            id="notes"
+                                            rows={3}
+                                            className="block w-full rounded-md border border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                                            placeholder="Patient need to take paracetamol 3 times a day, and need to rest for 2 days"
+                                            defaultValue={""}
+                                            {...register("note", { required: true })}
+                                        />
+                                    </div>
+                                </div>
+
+                                <div className="col-span-6">
+                                    <label
+                                        htmlFor="fee"
+                                        className="block text-sm font-medium text-gray-700"
+                                    >
+                                        Diagnosis {redAsterisk}
+                                    </label>
+
+                                    <Input
+                                        type="text"
+                                        id="diagnosis"
+                                        {...register('diagnosis', {
+                                            required: true,
+                                        })}
+                                        placeholder="Open Wound"
+                                        className="mt-1"
+                                    />
+                                </div>
+
+                                <div className="col-span-6">
+                                    <label className="block text-sm font-medium text-gray-700">
+                                        Foto luka (optional){" "}
+                                    </label>
+                                    <div className="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pb-6 pt-5">
+                                        {previewCheckUpAttachments.length > 0 ? (
+                                            <div className="mt-3.5 grid gap-2">
+                                                <Attachments
+                                                    attachments={previewCheckUpAttachments.map(
+                                                        (attachment) => attachment.attachment
+                                                    )}
+                                                    onRemoveAttachment={onRemoveAttachment}
+                                                />
+                                            </div>
+                                        ) : (
+                                            <div className="space-y-1 text-center">
+                                                <svg
+                                                    className="mx-auto h-12 w-12 text-gray-400"
+                                                    stroke="currentColor"
+                                                    fill="none"
+                                                    viewBox="0 0 48 48"
+                                                    aria-hidden="true"
+                                                >
+                                                    <path
+                                                        d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                                                        strokeWidth={2}
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                    />
+                                                </svg>
+                                                <div className="flex text-sm text-gray-600">
+                                                    <label
+                                                        htmlFor="file-upload"
+                                                        className="relative cursor-pointer rounded-md bg-white font-medium text-blue-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-blue-500 focus-within:ring-offset-2 hover:text-blue-500"
+                                                    >
+                                                        <span>Upload a file</span>
+
+                                                        <input
+                                                            id="file-upload"
+                                                            name="file-upload"
+                                                            type="file"
+                                                            className="sr-only"
+                                                            //only accept image files
+                                                            accept="image/*"
+                                                            onChange={onFilesChange}
+                                                            multiple
+                                                        />
+                                                    </label>
+                                                    <p className="pl-1">or drag and drop</p>
+                                                </div>
+                                                <p className="text-xs text-gray-500">
+                                                    PNG, JPG, GIF up to 10MB
+                                                </p>
+                                            </div>
+                                        )}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            {/* <div className="md:grid md:grid-cols-3 md:gap-6">
                 <div className="md:col-span-1">
                     <h3 className="text-lg font-medium leading-6 text-gray-900">
                         Check up
@@ -263,7 +477,7 @@ export function CheckupForm() {
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </div>
 
     )
