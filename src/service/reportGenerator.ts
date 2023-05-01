7b432ea32b91c2022596b37195b77878808dff51
Fatwa <fatwaaryasatyaakbar@gmail.com>
2023-05-01 14:05:50 +0700

    ignore autotable

--- src/service/reportGenerator.ts
+++ src/service/reportGenerator.ts
@@ -4,39 +4,38 @@ import dayjs from "dayjs";
 import { RouterOutputs } from "@/utils/api";
 import { rupiah } from "@/utils/intlformat";
 
- type ReportData = RouterOutputs['record']['getRecordReports']
- type ReportColumn = ReportData[number];
+type ReportData = RouterOutputs["record"]["getRecordReports"];
+type ReportColumn = ReportData[number];
 
+const generatePDF = (patients: ReportData) => {
+  const doc = new jsPDF();
 
-const generatePDF = (patients : ReportData) => {
-    const doc = new jsPDF();
+  const tblColumn = [
+    "Tanggal Pemeriksaan",
+    "Nama Pasien",
+    "Alamat Pasien",
+    "Nomor Telepon",
+    "Nama Perawat",
+    "Biaya",
+  ];
+  const tblRows: ReportColumn[keyof ReportColumn][][] = [];
 
-
-    const tblColumn = [
-        'Tanggal Pemeriksaan',
-        'Nama Pasien',
-        'Alamat Pasien',
-        'Nomor Telepon',
-        'Nama Perawat',
-        'Biaya'
+  patients.forEach((data) => {
+    const patientsData = [
+      dayjs(data?.createdAt).format("DD MMMM YYYY"),
+      data?.patient.name,
+      data?.patient.address,
+      data?.patient.phone,
+      data?.patient.user.name,
+      rupiah.format(data?.pay),
     ];
-    const tblRows: (ReportColumn[keyof ReportColumn])[][] = [];
-    
-    patients.forEach(data => {
-        const patientsData = [  
-            dayjs(data?.createdAt).format('DD MMMM YYYY'),
-            data?.patient.name,
-            data?.patient.address,
-            data?.patient.phone,
-            data?.patient.user.name,
-            rupiah.format(data?.pay)
-        ]
-        tblRows.push(patientsData)
-    })
+    tblRows.push(patientsData);
+  });
 
-    doc.autoTable(tblColumn, tblRows, { startY: 20 })
-    const dateFormat = dayjs(new Date()).format('DD MMMM YYYY')
-    doc.save(`report_${dateFormat}.pdf`);
-}
+  //@ts-ignore
+  doc.autoTable(tblColumn, tblRows, { startY: 20 });
+  const dateFormat = dayjs(new Date()).format("DD MMMM YYYY");
+  doc.save(`report_${dateFormat}.pdf`);
+};
 
 export default generatePDF;
