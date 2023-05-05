3ce7bc814fdf1a254eb48566259a0da5192c94ef
Fatwa <fatwa.aryasatya@dansmultipro.com>
2023-05-05 13:40:44 +0700

    fix undefined iterator

--- src/components/ui/breadcrumb.tsx
+++ src/components/ui/breadcrumb.tsx
@@ -8,20 +8,20 @@ export default function Breadcrumbs() {
   const breadcrumbs = useMemo((
     function generateBreadcrumbs() {
       const asPathWithoutQuery = router.asPath.split("?")[0];
-  
+
       // remove "/path1/path2/path3" => ["path1", "path2", "path3"]
       const asPathNestedRoutes = asPathWithoutQuery?.split("/").filter(p => p.length > 0);
-  
+
       // build crumb object
       const crumbList = asPathNestedRoutes?.map((subpath, i) => {
         const href = "/" + asPathNestedRoutes.slice(0, i + 1).join("/");
         const text = subpath;
-        
-        return {href, text};
+
+        return { href, text };
       })
-  
+
       // create default path wich is "Home"
-      return [{href: "/", text: "Home"}, ...crumbList];
+      return [{ href: "/", text: "Home" }, ...crumbList as { href: string, text: string }[]];
     }
   ), [router.asPath])
 
@@ -46,9 +46,9 @@ interface CrumbProps {
   last?: boolean;
 }
 
-const Crumb : React.FC<CrumbProps> = ({ text, href, last=false}) => {
+const Crumb: React.FC<CrumbProps> = ({ text, href, last = false }) => {
 
-  if(last) {
+  if (last) {
     return (
       <>
         <span className="ml-4 text-sm font-medium text-gray-500 capitalize">{text}</span>
@@ -58,25 +58,25 @@ const Crumb : React.FC<CrumbProps> = ({ text, href, last=false}) => {
 
   return (
     <>
-        <a href={href} className="ml-4 text-sm font-medium text-gray-400 hover:text-gray-700">
-          {
-            text === "Home" ? <HomeIcon className="flex-shrink-0 h-5 w-5" /> : 
+      <a href={href} className="ml-4 text-sm font-medium text-gray-400 hover:text-gray-700">
+        {
+          text === "Home" ? <HomeIcon className="flex-shrink-0 h-5 w-5" /> :
             <span className="capitalize">
               {text}
             </span>
-          }
-        </a>
-        <svg
-            className="flex-shrink-0 w-6 h-full text-gray-200"
-            viewBox="0 0 24 44"
-            preserveAspectRatio="none"
-            fill="currentColor"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-          >
-          <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
-        </svg>
+        }
+      </a>
+      <svg
+        className="flex-shrink-0 w-6 h-full text-gray-200"
+        viewBox="0 0 24 44"
+        preserveAspectRatio="none"
+        fill="currentColor"
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+      >
+        <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
+      </svg>
     </>
-    
+
   )
 }
