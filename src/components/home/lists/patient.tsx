2984bc18e028fc22b3aa0628ba12b4ff44c3f5e9
Fatwa <fatwaaryasatyaakbar@gmail.com>
2023-05-27 00:57:52 +0700

    fix fuzzy filter

--- src/components/home/lists/patient.tsx
+++ src/components/home/lists/patient.tsx
@@ -43,33 +43,33 @@ import { UserPlus, MoreHorizontal } from "lucide-react";
 //     }
 // }
 
-// export const fuzzyFilter: FilterFn<any> = (row, columnId, value, addMeta) => {
-//     // Rank the item
-//     const itemRank = rankItem(row.getValue(columnId), value);
+export const fuzzyFilter: FilterFn<any> = (row, columnId, value, addMeta) => {
+  // Rank the item
+  const itemRank = rankItem(row.getValue(columnId), value);
 
-//     // Store the itemRank info
-//     addMeta({
-//         itemRank,
-//     });
+  // Store the itemRank info
+  addMeta({
+    itemRank,
+  });
 
-//     // Return if the item should be filtered in/out
-//     return itemRank.passed;
-// };
+  // Return if the item should be filtered in/out
+  return itemRank.passed;
+};
 
-// export const fuzzySort: SortingFn<any> = (rowA, rowB, columnId) => {
-//     let dir = 0;
+export const fuzzySort: SortingFn<any> = (rowA, rowB, columnId) => {
+  let dir = 0;
 
-//     // Only sort by rank if the column has ranking information
-//     if (rowA.columnFiltersMeta[columnId]) {
-//         dir = compareItems(
-//             rowA.columnFiltersMeta[columnId]?.itemRank!,
-//             rowB.columnFiltersMeta[columnId]?.itemRank!
-//         );
-//     }
+  // Only sort by rank if the column has ranking information
+  if (rowA.columnFiltersMeta[columnId]) {
+    dir = compareItems(
+      rowA.columnFiltersMeta[columnId]?.itemRank!,
+      rowB.columnFiltersMeta[columnId]?.itemRank!
+    );
+  }
 
-//     // Provide an alphanumeric fallback for when the item ranks are equal
-//     return dir === 0 ? sortingFns.alphanumeric(rowA, rowB, columnId) : dir;
-// };
+  // Provide an alphanumeric fallback for when the item ranks are equal
+  return dir === 0 ? sortingFns.alphanumeric(rowA, rowB, columnId) : dir;
+};
 
 const columnHelper = createColumnHelper<PatientColumn>();
 
