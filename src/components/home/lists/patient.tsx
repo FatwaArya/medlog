0b3aca34402566f04e2b3d3b0c51d16618d5d5af
Fatwa <fatwaaryasatyaakbar@gmail.com>
2023-05-27 01:04:50 +0700

    wait for table migration comp

--- src/components/home/lists/patient.tsx
+++ src/components/home/lists/patient.tsx
@@ -5,7 +5,7 @@ import dayjs from "dayjs";
 import { Button } from "@/components/ui/button";
 import relativeTime from "dayjs/plugin/relativeTime";
 import "dayjs/locale/id"; // ES 2015
-import { createColumnHelper } from "@tanstack/react-table";
+import { type FilterFn, createColumnHelper, SortingFn } from "@tanstack/react-table";
 import { useEffect, useState } from "react";
 import { Checkbox } from "@/components/ui/checkbox";
 import Link from "next/link";
@@ -33,43 +33,6 @@ import {
 } from "@tanstack/match-sorter-utils";
 import { Spinner } from "@/components/ui/loading-overlay";
 import { UserPlus, MoreHorizontal } from "lucide-react";
-
-// declare module "@tanstack/table-core" {
-//     interface FilterFns {
-//         fuzzy: FilterFn<unknown>;
-//     }
-//     interface FilterMeta {
-//         itemRank: RankingInfo;
-//     }
-// }
-
-export const fuzzyFilter: FilterFn<any> = (row, columnId, value, addMeta) => {
-  // Rank the item
-  const itemRank = rankItem(row.getValue(columnId), value);
-
-  // Store the itemRank info
-  addMeta({
-    itemRank,
-  });
-
-  // Return if the item should be filtered in/out
-  return itemRank.passed;
-};
-
-export const fuzzySort: SortingFn<any> = (rowA, rowB, columnId) => {
-  let dir = 0;
-
-  // Only sort by rank if the column has ranking information
-  if (rowA.columnFiltersMeta[columnId]) {
-    dir = compareItems(
-      rowA.columnFiltersMeta[columnId]?.itemRank!,
-      rowB.columnFiltersMeta[columnId]?.itemRank!
-    );
-  }
-
-  // Provide an alphanumeric fallback for when the item ranks are equal
-  return dir === 0 ? sortingFns.alphanumeric(rowA, rowB, columnId) : dir;
-};
 
 const columnHelper = createColumnHelper<PatientColumn>();
 
