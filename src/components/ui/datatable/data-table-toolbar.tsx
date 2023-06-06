0d88b38c7c0801827dc8eb9c86035a65e4b5b2bb
rayhan889 <raydev1604@gmail.com>
2023-06-06 11:37:22 +0700

    datatable-faceted finish up

--- src/components/ui/datatable/data-table-toolbar.tsx
+++ src/components/ui/datatable/data-table-toolbar.tsx
@@ -1,76 +1,78 @@
 "use client"
 
 import { Table } from "@tanstack/table-core";
-import { SearchIcon, X } from "lucide-react";
+import { SearchIcon, X, UserPlus } from "lucide-react";
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { DataTableViewOptions } from "./data-table-viewOptions";
+import { DataTableViewOptions } from "@/components/ui/datatable/data-table-viewOptions";
+import { DataTableFacetedFilter } from "@/components/ui/datatable/data-table-faceted-filter";
+
 import { useEffect, useState } from "react";
+
+import { statuses } from "@/pages/dashboard/accounts-management/data/data";
 
 interface DataTableToolbarProps<TData> {
     table: Table<TData>
+    filter?: string
+    filterTitle?: string
+    isFacetedFilter?: boolean
+    columnViews?: { title: string }[]
+    href?: string
 }
 
 export function DataTableToolbar<TData>({
-    table
+    table, filter, isFacetedFilter, columnViews, href, filterTitle
 }: DataTableToolbarProps<TData>){
-    const [globalFilter, setGlobalFilter] = useState("");
-
     const isFiltered = 
         table.getPreFilteredRowModel().rows.length > 
         table.getFilteredRowModel().rows.length;
 
     return (
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between w-full">
+            <div className="flex items-center gap-4">
             <div className="flex flex-1 items-center space-x-2">
-                <DebouncedInput
-                    value={globalFilter ?? ""}
-                    onChange={(value) => setGlobalFilter(String(value))}
-                    className="w-full sm:w-64"
-                    placeholder="Search"
-                />
+                {
+                    filter && (
+                        <Input
+                            placeholder={`Filter ${filterTitle}...`}
+                            value={(table.getColumn(filter)?.getFilterValue() as string) ?? ""}
+                            onChange={(e) => 
+                                table.getColumn(filter)?.setFilterValue(e.target.value)
+                            }
+                            className="h-8 w-[150px] lg:w-[250px]"
+                        />
+                    )
+                }
+                {isFacetedFilter && (
+                    table.getColumn("status") && (
+                        <DataTableFacetedFilter
+                            column={table.getColumn("status")}
+                            title="Status"
+                            options={statuses}
+                        />
+                    )
+                )}
+                {isFiltered && (
+                    <Button
+                        variant="ghost"
+                        onClick={() => table.resetColumnFilters()}
+                        className="h-8 px-2 lg:px-3"
+                    >
+                        Reset
+                        <X className="ml-2 h-4 w-4" />
+                    </Button>
+                )}
             </div>
+            {
+                href && (
+                    <Button variant='outline' className="relative mt-1 rounded-md shadow-sm" href={href ?? "#"}>
+                        <UserPlus className="h-5 w-5 text-gray-400" />
+                    </Button>
+                )
+            }
+            </div>
+            <DataTableViewOptions columnViews={columnViews} table={table} />
         </div>
     )
-}
-
-export function DebouncedInput({
-    value: initialValue,
-    onChange,
-    debounce = 200,
-    ...props
-}: {
-    value: string | number;
-    onChange: (value: string | number) => void;
-    debounce?: number;
-} & Omit<React.InputHTMLAttributes<HTMLInputElement>, "onChange">) {
-    const [value, setValue] = useState(initialValue);
-
-    useEffect(() => {
-        setValue(initialValue);
-    }, [initialValue]);
-
-    useEffect(() => {
-        const timeout = setTimeout(() => {
-            onChange(value);
-        }, debounce);
-
-        return () => clearTimeout(timeout);
-    }, [value]);
-
-    return (
-        <>
-            <div className="relative rounded-md shadow-sm">
-                <Input
-                    {...props}
-                    value={value}
-                    onChange={(e) => setValue(e.target.value)}
-                />
-                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
-                    <SearchIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                </div>
-            </div>
-        </>
-    );
 }
