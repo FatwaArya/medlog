d1b60cce704194d69421b906795840222a827648
Fatwa <fatwaaryasatyaakbar@gmail.com>
2023-07-27 23:15:24 +0700

    fix (boosted db): add boostedPrisma on ssgHelper

--- src/server/api/helpers/ssgHelper.ts
+++ src/server/api/helpers/ssgHelper.ts
@@ -1,12 +1,12 @@
 import { createServerSideHelpers } from "@trpc/react-query/server";
 import { appRouter } from "../root";
-import { prisma } from "@/server/db";
+import { boostedPrisma, prisma } from "@/server/db";
 import superjson from "superjson";
 import type { Session } from "next-auth";
 
 export const generateSSGHelper = () =>
   createServerSideHelpers({
     router: appRouter,
-    ctx: { prisma, session: null },
+    ctx: { prisma, boostedPrisma, session: null },
     transformer: superjson,
   });
