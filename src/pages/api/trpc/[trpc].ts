0f6bb75058298fd562196f6234dde26537809ca8
Fatwa <fatwaaryasatyaakbar@gmail.com>
2023-09-01 22:34:15 +0700

    disable batching

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -3,21 +3,23 @@ import { createNextApiHandler } from "@trpc/server/adapters/next";
 import { env } from "@/env.mjs";
 import { createTRPCContext } from "@/server/api/trpc";
 import { appRouter } from "@/server/api/root";
-import { withAxiom } from 'next-axiom'
-
+import { withAxiom } from "next-axiom";
 
 // export API handler
 export default withAxiom(
   createNextApiHandler({
-  router: appRouter,
-  createContext: createTRPCContext,
-  onError:
-    env.NODE_ENV === "development"
-      ? ({ path, error }) => {
-          console.error(
-            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
-          );
-        }
-      : undefined,
-})
-)
+    router: appRouter,
+    createContext: createTRPCContext,
+    onError:
+      env.NODE_ENV === "development"
+        ? ({ path, error }) => {
+            console.error(
+              `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
+            );
+          }
+        : undefined,
+    batching: {
+      enabled: false,
+    },
+  }),
+);
